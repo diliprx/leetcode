875a72e3eea637d383309092b3a043ca0e98fb7e
Dilip Kumar V <157562193+diliprx@users.noreply.github.com>
2025-09-17 19:06:42 +0530

    Time: 2 ms (96.79%), Space: 41.7 MB (74.67%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>b=new Stack<>();
+        for(char a:s.toCharArray()){
+            if(a== '('||a=='[' || a=='{'){
+                b.push(a);
+            }
+            else{
+                if(b.isEmpty()){
+                    return false;
+                }
+                char c=b.pop();
+                if(a==')' && c != '('){
+                    return false;
+                }
+                if(a==']' && c != '['){
+                    return false;
+                }
+                if(a=='}' && c != '{'){
+                    return false;
+                }
+            }
+        }
+        return b.isEmpty();
+        
+    }
+}
