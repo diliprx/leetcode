f8d44bf826bf7c39586d934ccd79b63e1cfb36bd
Dilip Kumar V <157562193+diliprx@users.noreply.github.com>
2025-09-21 22:42:26 +0530

    Time: 0 ms (100%), Space: 41.9 MB (73.46%) - LeetHub

---
+++ 3320-maximum-number-of-operations-with-the-same-score-i/3320-maximum-number-of-operations-with-the-same-score-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxOperations(int[] nums) {
+        int a=nums[0]+nums[1];
+        int b=1;
+        for(int i=2;i<nums.length-1;i+=2){
+            if(nums[i]+nums[i+1]==a){
+                b++;
+            }
+            else break;
+        }
+        return b;
+    }
+}
