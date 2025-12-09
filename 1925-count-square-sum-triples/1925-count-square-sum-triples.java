62227467da5cfbcd30bf4436221dab71b3df8859
Dilip Kumar V <157562193+diliprx@users.noreply.github.com>
2025-12-09 18:28:44 +0530

    Time: 13 ms (57.26%), Space: 41.7 MB (68.67%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -3,9 +3,7 @@ class Solution {
       int count =0;
       for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-           int a=i*i;
-           int b=j*j;
-           int c=a+b;
+           int c=i*i+j*j;
            int d=(int)Math.sqrt(c);
            if(d<=n && d*d==c){
             count++;
