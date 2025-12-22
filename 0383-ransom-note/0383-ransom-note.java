4f400c0a4d672945669e53f1873c348865a624ba
Dilip Kumar V <157562193+diliprx@users.noreply.github.com>
2025-12-22 22:04:12 +0530

    Time: 1 ms (99.81%), Space: 46.2 MB (67.2%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length()>magazine.length()){
+            return false;
+        }
+        int []a=new int[26];
+        for(char c:magazine.toCharArray()){
+            a[c-'a']++;
+        }
+        for(char c:ransomNote.toCharArray()){
+            if(a[c-'a']==0){
+                 return false;
+            }
+            a[c-'a']--;
+        }
+        return true;
+    }
+}
