9cb473a244cef28810065b6bd451f14b12e00772
Dilip Kumar V <157562193+diliprx@users.noreply.github.com>
2026-03-25 11:24:54 +0530

    Time: 1 ms (78.09%), Space: 42.4 MB (60.97%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n>9){
+            int r=0;
+            while(n>0){
+                r+=((n%10)*(n%10));
+                n/=10;
+            }
+            n=r;
+        }
+        if(n==1 || n==7){
+            return true;
+        }
+        return false;
+    }
+}
