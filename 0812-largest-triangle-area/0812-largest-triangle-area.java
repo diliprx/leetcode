5b1e1f5129da578f6105e735fa08400eb07a9bb1
Dilip Kumar V <157562193+diliprx@users.noreply.github.com>
2026-06-25 12:01:44 +0530

    Time: 9 ms (61%), Space: 45.4 MB (89.28%) - LeetHub

---
+++ 0812-largest-triangle-area/0812-largest-triangle-area.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        int n=points.length;
+        if(n<3){
+            return 0.00;
+        }
+        long max=0;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
+                    long x1=points[i][0],y1=points[i][1];
+                    long x2=points[j][0],y2=points[j][1];
+                    long x3=points[k][0],y3=points[k][1];
+                    long current=Math.abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+                    if(current>max){
+                        max=current;
+                    }
+                }
+            }
+        }
+        return max/2.0;
+        
+    }
+}
