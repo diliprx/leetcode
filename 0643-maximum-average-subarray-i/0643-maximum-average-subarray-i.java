fa28e0430506c98695471ecdf3c85f5dca37689c
Dilip Kumar V <157562193+diliprx@users.noreply.github.com>
2026-08-17 09:43:22 +0530

    Time: 2610 ms (5%), Space: 69.6 MB (63.54%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,24 +1,16 @@
-class Solution { 
-    public double findMaxAverage(int[] nums, int k) { 
-        double kd = (double)k; 
-        // 1. Initialize b to the lowest possible value to handle negative answers
-        double b = -Double.MAX_VALUE; 
-        
-        // 2. Change < to <= to include the final window
-        for(int i = 0; i <= nums.length - k; i++){ 
-            int j = i + k - 1; 
-            
-            // 3. Reset the sum 'a' for every new window
-            int a = 0; 
-            
-            for(int m = i; m <= j; m++){ 
-                a += nums[m]; 
-            } 
-            
-            // 4. Calculate the average using a double cast to prevent integer truncation
-            double cur = (double)a / kd; 
-            b = Math.max(b, cur); 
-        } 
-        return b; 
-    } 
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double kd=(double)k;
+        double b=-Double.MAX_VALUE;
+        for(int i=0;i<=nums.length-k;i++){
+            int j=i+k-1;
+            int a=0;
+            for(int m=i;m<=j;m++){
+                a+=nums[m];
+            }
+            double cur=(double)a/kd;
+            b=Math.max(b,cur);
+        }
+        return b;
+    }
 }
